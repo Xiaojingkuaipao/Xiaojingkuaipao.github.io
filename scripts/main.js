9d38bcea0ba39196a9ff38784d77a2208b3571ef
Zhijing Xin <zhijingxin@stu.cqu.edu.cn>
2025-08-13 14:34:29 +0800

    the first version by mdn

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/tom.jpg") {
+    myImage.setAttribute("src", "images/tom-2.jpg");
+  } else {
+    myImage.setAttribute("src", "images/tom.jpg");
+  }
+};
+
+let myHeading = document.querySelector("h1");
+let myButton = document.querySelector("button");
+let myName;
+
+function setUserName() {
+  myName = prompt("Please enter your name");
+  localStorage.setItem("name", myName);
+}
+
+function refreshHeading() {
+  let storedName = localStorage.getItem("name");
+  if (storedName) {
+    myHeading.textContent = `Zhijing Xin's Site, ${storedName}`;
+  } else {
+    myHeading.textContent = "Zhijing Xin's Website";
+  }
+}
+
+myButton.onclick = function () {
+  setUserName();
+  refreshHeading();
+};
